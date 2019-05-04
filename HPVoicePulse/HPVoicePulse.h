00e3f2a8c44e4444b9f33446e3b37ef8c5850fc1
Hoang <hoangq@firestak.com>
2019-05-04 08:56:58 +0700

    + Init project

---
+++ HPVoicePulse/HPVoicePulse.h
@@ -0,0 +1,19 @@
+//
+//  HPVoicePulse.h
+//  HPVoicePulse
+//
+//  Created by Hoang on 5/4/19.
+//  Copyright © 2019 Quang Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HPVoicePulse.
+FOUNDATION_EXPORT double HPVoicePulseVersionNumber;
+
+//! Project version string for HPVoicePulse.
+FOUNDATION_EXPORT const unsigned char HPVoicePulseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HPVoicePulse/PublicHeader.h>
+
+
